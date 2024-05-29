3c9589dd8ba041af801d22b3fd42a4301e68be7c
Tony Huang <117428802+2549486373@users.noreply.github.com>
2024-05-29 13:06:35 -0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Covid_19_prison/', // Set the base path for GitHub Pages deployment
-  plugins: [svelte()],
-});
