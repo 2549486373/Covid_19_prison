0226abadc842daaa933a6cfe2e0d47963f3864af
Tony Huang <huangtony698@gmail.com>
2024-05-29 13:13:30 -0700

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Covid-visualization/', // Set the base path for GitHub Pages deployment
-  plugins: [svelte()],
-});
