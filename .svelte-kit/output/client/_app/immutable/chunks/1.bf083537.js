8f7015592d1d20b77303d68e222b2379e7129c09
Tony Huang <117428802+2549486373@users.noreply.github.com>
2024-05-29 12:21:09 -0700

    Delete .svelte-kit directory

--- .svelte-kit/output/client/_app/immutable/chunks/1.bf083537.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.36bb9332.js";export{t as component};
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const file = '_app/immutable/entry/_page.svelte.13d43ad7.js';
-export const imports = ["_app/immutable/entry/_page.svelte.13d43ad7.js","_app/immutable/chunks/index.8802f197.js"];
-export const stylesheets = ["_app/immutable/assets/_page.622f6e66.css"];
-export const fonts = [];
