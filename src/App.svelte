067b8aaabb40c7898c17c2ab7c824d41cdac105e
Tony Huang <huangtony698@gmail.com>
2024-06-08 13:29:29 -0700

    tony

--- src/App.svelte
+++ src/App.svelte
@@ -328,6 +328,13 @@
 </Scroller>
 
 <style>
+  html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
   .container {
     display: flex;
     overflow: hidden; 
